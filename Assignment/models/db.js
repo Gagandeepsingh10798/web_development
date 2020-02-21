3679a6fd8af8c55edf3c01d7fc3b0eaccf044ba5
Gagandeep Singh <gs593513@gmail.com>
2020-02-21 13:03:23 +0530

    Add files via upload

---
+++ Assignment/models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const string = require("../config/config.json");
+const dbPath = string[0].db_url;
+mongoose.connect(dbPath, {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", () => {
+    console.log("> error occurred from the database");
+});
+db.once("open", () => {
+    console.log("> successfully opened the database");
+});
+
+
+module.exports = mongoose;
