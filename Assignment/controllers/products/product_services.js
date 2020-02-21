230a7bce09d047e8b5e9fff2ccde64b8558c2103
Gagandeep Singh <gs593513@gmail.com>
2020-02-22 01:43:14 +0530

    Add files via upload

--- Assignment/controllers/products/product_services.js
+++ Assignment/controllers/products/product_services.js
@@ -19,23 +19,8 @@ db_product.findOne({p_id: content.p_id}, function (err, docs) {
 db_user.findOne({ _id: u_id}, function (err, docs) {
   if(docs !== null && docs !== undefined){
 
-    db_review.find({product_id: content.p_id},function(err, docs) {
-    if(docs.length){
-    var obj = new db_product({
-      p_id: content.p_id,
-      p_name: content.p_name,
-      p_desc: content.p_desc,
-      p_image:content.p_image,
-      obj_id: req.params.u_id,
-      reviews: docs
-    })
 
-    obj.save((err,data)=>{
-      if(!err){res.send('product entered')}
-      else{res.send(err)}
-    })}
-    else{
-        var obj = new db_product({
+      var obj = new db_product({
       p_id: content.p_id,
       p_name: content.p_name,
       p_desc: content.p_desc,
@@ -48,9 +33,7 @@ db_user.findOne({ _id: u_id}, function (err, docs) {
       if(!err){res.send('product entered')}
       else{res.send(err)}
     })
-    }
-      
-  })}
+ }
   else{
     res.send("user not exist");
   }
@@ -107,12 +90,24 @@ exports.update_product = (req,res)=>{
 exports.show_user_products = (req,res)=>{
     
     var u_id = req.params.id
-    db_product.find({obj_id : u_id},function(err,doc){
-       if(doc.length){
-           res.send(doc)
+    db_product.findOne({obj_id : u_id},function(err,doc){
+       if(doc!==null && doc!==undefined ){
+        db_review.find({product_id: doc['p_id']},function(err,docss){
+          if(docss.length){
+            var keys =['_id','review_id','review_message'],value = [docss[0]['_id'],docss[0]['review_id'],docss[0]['review_message']] ,obj={}
+            keys.forEach(function (k, i) {
+              obj[k] = value[i];
+          })
+            doc.reviews = [obj]
+            res.send(doc)
+          }
+          else{
+            res.send(doc)
+          }
+        })
        }       
        else{
-           res.send('no product for uthis user id')
+           res.send('no product for this user id')
        }
     })
     
