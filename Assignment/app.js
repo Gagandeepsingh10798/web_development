011cd17d343d8c654e926736cc5cc1e90b5c3460
Gagandeep Singh <gs593513@gmail.com>
2020-02-25 20:17:23 +0530

    Add files via upload

--- Assignment/app.js
+++ Assignment/app.js
@@ -1,26 +1,35 @@
 const bodyParser = require('body-parser');
 const user = require('./controllers/user/user');
+const register = require('./controllers/register/register');
 const login = require('./controllers/login/login');
 const product = require('./controllers/products/product');
 const review = require('./controllers/reviews/review');
+var jwt = require('jsonwebtoken')
 module.exports = function(app) {
 
 
     app.use(bodyParser.text())
 
-    app.use('/api/auth/register',user);
+    app.use('/api/auth/register',register);
     app.use('/api/auth',login);
-
+    app.use('/api/user',user);
 
     const checkAuth = (req,res,next)=>{
         if(!req.headers.token){
           res.send('unauthorized user')
         }
-        
-          next();
-    }
+        else{
+          jwt.verify(req.headers.token, 'supersecret', function(err, decoded){
+            if(!err){
+              next();
+            } 
+            else {
+              res.send('unauthorized access');
+            }
+          })}}   
+
     app.use(checkAuth)
-    app.use('/api/product',product);
+    app.use('/api/products',product);
     app.use('/api/review',review);
 
     //invalid url
