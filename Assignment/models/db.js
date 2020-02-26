187dd88ecf7730b8c4921f8b12550a50000c25f9
Gagandeep Singh <gs593513@gmail.com>
2020-02-26 05:46:54 +0530

    Add files via upload

--- Assignment/models/db.js
+++ Assignment/models/db.js
@@ -4,6 +4,7 @@ const dbPath = string[0].db_url;
 mongoose.connect(dbPath, {
     useNewUrlParser: true, useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
 db.on("error", () => {
     console.log("> error occurred from the database");
