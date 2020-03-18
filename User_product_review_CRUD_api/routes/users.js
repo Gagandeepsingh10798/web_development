386eaed9424663b9355c02ddc124c5e4b482aa79
Gagandeep Singh <gs593513@gmail.com>
2020-03-18 06:37:07 +0530

    Add files via upload

--- User_product_review_CRUD_api/routes/users.js
+++ User_product_review_CRUD_api/routes/users.js
@@ -2,7 +2,7 @@ try{
 
 const express = require('express');
 var router = express.Router();
-
+var static = require('../static')
 const userController = require('../controllers/users.controller') 
 
 
@@ -19,15 +19,26 @@ router.get('/all', function (req, res) {
         userController.users(req,res)
     
 })
+
+router.get('/get/:id', function (req, res) {
+   
+  userController.get_userr(req,res)
+
+})
+
   
   
-router.delete('/', function (req, res) {
+router.delete('/delete/:id', function (req, res) {
     userController.delete_user(req,res)
 })
 
 
-router.put('/', function (req, res) {
+router.put('/', static.validations.auth,function (req, res) {
     userController.update_user(req,res)
+})
+
+router.put('/:id', static.validations.auth,function (req, res) {
+  userController.update_userr(req,res)
 })
   
   
